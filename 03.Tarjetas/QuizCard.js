356984937cf91e05dc02d3bcc88ceca7eda451d0
Aaron <aaron@intevolution.mx>
2020-11-20 14:47:39 -0600

    final

--- 03.Tarjetas/QuizCard.js
+++ 03.Tarjetas/QuizCard.js
@@ -30,5 +30,44 @@ quizCard = {
         }
     ]
   }
+
+//   quizCard = 
+//   {
+//     "type": "AdaptiveCard",
+//     "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
+//     "version": "1.2",
+//     "body": [
+//         {
+//             "type": "TextBlock",
+//             "text": "Hola, mi nombre es InteBot y me gustan los perritos",
+//             "wrap": true,            
+//             "size": "Medium",
+//             "color": "Warning",
+//             "weight": "Bolder"
+//         },
+//         {
+//             "type": "ActionSet",
+//             "actions": [
+//                 {
+//                     "type": "Action.Submit",
+//                     "title": "Pedir mi nombre",
+//                     "data": 'Hola'
+//                 }
+//             ]
+//         },
+//         {
+//             "type": "Image",
+//             "url": "https://www.hola.com/imagenes/estar-bien/20190820147813/razas-perros-pequenos-parecen-grandes/0-711-550/razas-perro-pequenos-grandes-m.jpg",
+//             "size": "Large",
+//             "style": "Person",
+//             "horizontalAlignment": "Center"
+//         },
+//         {
+//             "type": "Image",
+//             "url": "https://media.tenor.com/images/d2d53ed3fbab71762790a7e95f6aae47/tenor.gif",
+//             "horizontalAlignment": "Center"
+//         }
+//     ]
+// }
   
   module.exports =  quizCard;
